14eb49e77b47fe6bb5d345e0e801852ebef1a4a7
Bruno Paulin <bruno.p-@hotmail.com>
2023-06-22 21:44:30 -0300

    fix:css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         fill
         style={{ objectFit: 'cover' }}
       />
-      <main className="relative z-10 flex h-screen flex-col gap-2 overflow-y-scroll p-5 md:m-auto">
+      <main className="relative z-10 flex h-screen flex-col gap-2 overflow-y-scroll p-5 md:m-auto md:max-w-[900px]">
         <h1 className="text-center text-2xl font-bold text-black md:text-4xl">
           Carcassone Counter
         </h1>
