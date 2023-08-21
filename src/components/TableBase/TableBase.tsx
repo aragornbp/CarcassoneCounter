cf5e2c43dc8a07c24d07cdb778c32179c55d51e2
Bruno Paulin <bruno.p-@hotmail.com>
2023-08-21 20:56:35 -0300

    teste css rotate

--- src/components/TableBase/TableBase.tsx
+++ src/components/TableBase/TableBase.tsx
@@ -23,61 +23,63 @@ const TableBase = ({
   setMemorian,
 }: iTableBase) => {
   return (
-    <div className="h-max-[400px] m-auto mt-4 h-fit w-full max-w-[1120px] overflow-x-auto rounded-xl border text-xl text-black">
-      <table className="w-full  rounded-xl bg-slate-400 opacity-90">
-        <thead className="whitespace-nowrap">
-          <td className=" p-1 text-center">Player</td>
-          <td className="w-[16%]  p-1 text-center">
-            <div className="flex flex-col items-center justify-center">
-              <GiMountainRoad size={24} />
-              Rua
-            </div>
-          </td>
-          <td className="w-[16%]  p-1 text-center">
-            <div className="flex flex-col items-center justify-center">
-              <MdOutlineCastle size={24} />
-              Cidade
-            </div>
-          </td>
-          <td className="w-[16%]  p-1 text-center">
-            <div className="flex flex-col items-center justify-center">
-              <Church strokeWidth={1.25} />
-              igreja
-            </div>
-          </td>
-          <td className="w-[16%] p-1 text-center">
-            <div className="flex flex-col items-center justify-center">
-              <GiFarmer size={24} />
-              Fazenda
-            </div>
-          </td>
-          <td className="w-[16%] p-1 text-center">
-            <div className="flex flex-col items-center justify-center">
-              <GiFairy size={24} />
-              Fada
-            </div>
-          </td>
-          <td className="w-full  p-1 text-center">Total</td>
-        </thead>
-        <tbody className="whitespace-nowrap">
-          {players?.length > 0 ? (
-            players.map((player, index) => (
-              <TableRow
-                key={index}
-                player={player}
-                valor={valor}
-                players={players}
-                setPlayers={setPlayers}
-                negativo={negativo}
-                setMemorian={setMemorian}
-              />
-            ))
-          ) : (
-            <h2>Nenhum jogador adicionado</h2>
-          )}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className="m-auto mt-40 h-fit w-full max-w-[1120px] rotate-90 rounded-xl border text-xl text-black md:rotate-0 md:overflow-x-auto">
+        <table className="w-full  rounded-xl bg-slate-400 opacity-90">
+          <thead className="whitespace-nowrap">
+            <td className=" p-1 text-center">Player</td>
+            <td className="w-[16%]  p-1 text-center">
+              <div className="flex flex-col items-center justify-center">
+                <GiMountainRoad size={24} />
+                Rua
+              </div>
+            </td>
+            <td className="w-[16%]  p-1 text-center">
+              <div className="flex flex-col items-center justify-center">
+                <MdOutlineCastle size={24} />
+                Cidade
+              </div>
+            </td>
+            <td className="w-[16%]  p-1 text-center">
+              <div className="flex flex-col items-center justify-center">
+                <Church strokeWidth={1.25} />
+                igreja
+              </div>
+            </td>
+            <td className="w-[16%] p-1 text-center">
+              <div className="flex flex-col items-center justify-center">
+                <GiFarmer size={24} />
+                Fazenda
+              </div>
+            </td>
+            <td className="w-[16%] p-1 text-center">
+              <div className="flex flex-col items-center justify-center">
+                <GiFairy size={24} />
+                Fada
+              </div>
+            </td>
+            <td className="w-full  p-1 text-center">Total</td>
+          </thead>
+          <tbody className="whitespace-nowrap">
+            {players?.length > 0 ? (
+              players.map((player, index) => (
+                <TableRow
+                  key={index}
+                  player={player}
+                  valor={valor}
+                  players={players}
+                  setPlayers={setPlayers}
+                  negativo={negativo}
+                  setMemorian={setMemorian}
+                />
+              ))
+            ) : (
+              <h2>Nenhum jogador adicionado</h2>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
